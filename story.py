4a841a0757f50b332aeaa5e872c5e914420996d3
Javier2C <juanortiz20500@gmail.com>
2026-08-15 23:46:28 +0000

    story

---
+++ story.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+color = input("Tell me a color: ")
+adjective = input("Give me an adjective: ")
+goal = input("A goal you would like to achieve: ")
+print()
+
+
+print(f"Hello, {name}!")
+print()
+print("This is your story:")
+print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
