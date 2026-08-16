3523df5882031235e194752285c91933fd5c9ab4
Javier2C <juanortiz20500@gmail.com>
2026-08-16 00:01:30 +0000

    yelling story

--- story.py
+++ story.py
@@ -1,11 +1,17 @@
-name = input("What is your name? ")
-color = input("Tell me a color: ")
-adjective = input("Give me an adjective: ")
-goal = input("A goal you would like to achieve: ")
+name = input("What is your name? ").strip().title()
+color = input("Tell me a color: ").strip().title()
+adjective = input("Give me an adjective: ").strip().title()
+goal = input("A goal you would like to achieve: ").strip().title()
 print()
 
 
 print(f"Hello, {name}!")
 print()
 print("This is your story:")
-print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+story = f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}."
+
+print(story)
+print()
+print("Yelling version:")
+print(story.upper())
