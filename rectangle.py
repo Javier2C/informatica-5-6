2e2dc48acb72f2e56550e8ed40810042df6fa0dc
Javier2C <juanortiz20500@gmail.com>
2026-08-18 16:21:33 +0000

    finished rectangle

--- rectangle.py
+++ rectangle.py
@@ -9,5 +9,11 @@ def main():
     p = (5 * 2) + (width * 2)
     print("perimeter:", p)
 
+    a = (5 * width)
+    print("area:", a)
+
+    d = (((5**2)+(width**2))**.5)
+    print("diagonal:", d)
+
 if __name__ == "__main__":
     main()
