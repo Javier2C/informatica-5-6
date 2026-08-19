57f19323208175a213f4ed1bd06412d6d2eefeab
Javier2C <juanortiz20500@gmail.com>
2026-08-19 20:21:01 +0000

    currency 2

--- currency.py
+++ currency.py
@@ -1,13 +1,9 @@
 def main():
-    c_money = int(input("Enter your remainder colombian pesos: "))
-    b_money = int(input("Enter your remainder brazilian reais: "))
-    p_money = int(input("Enter your remainder peruvian soles: "))
+    c = int(input("Enter your remainder colombian pesos: "))
+    b = int(input("Enter your remainder brazilian reais: "))
+    p = int(input("Enter your remainder peruvian soles: "))
 
-    usd = ((c_money / 3111.24) + (b_money / 0.19) + (p_money / 0.30))
-    print("USD:", usd)
-
-    mxn = (usd / 17.06)
-    print(round("MXN:", mxn), )
+    mxn = (c * 0.0054) + (b * 5.07) + (p * 3.28)
 
 if __name__ == "__main__":
     main()
