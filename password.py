136235602ec74e4e8e01294f49624f9f81d5862b
Javier2C <juanortiz20500@gmail.com>
2026-08-24 19:37:59 +0000

    password

---
+++ password.py
@@ -0,0 +1,17 @@
+def main():
+
+    password = "bigotito1"
+
+
+    attempt = input("enter your password:")
+    if attempt == password:
+        print("good boy")
+
+    print("alr the end")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
