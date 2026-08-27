62db994be8f965d7350236d33f63544c54948504
Javier2C <juanortiz20500@gmail.com>
2026-08-27 16:37:50 +0000

    test 1

---
+++ atmosphere.py
@@ -0,0 +1,46 @@
+def main():
+
+   layer = input(print("Descent atmosphere layer:"))
+
+        #voy a ponerlo doble por si ponen la primera letra en mayuscula para que el programa no se confunda
+        if layer == "exosphere":
+            print("your altitude level will be between 700–10,000 km")
+        elif layer == "Exosphere":
+            print("your altitude level will be between 700–10,000 km")
+
+        elif layer == "Thermosphere":
+            print("your altitude level will be between 85–700 km")
+        elif layer == "thermosphere"
+            print("your altitude level will be between 85–700 km")
+
+        elif layer == "Mesosphere":
+            print("your altitude level will be between 50–85 km")
+        elif layer == "mesosphere":
+            print("your altitude level will be between 50–85 km")
+
+        elif layer == "Stratosphere":
+            print("your altitude level will be between 12–50 km")
+        elif layer == "stratosphere":
+            print("your altitude level will be between 12–50 km")
+
+        elif layer == "Troposphere":
+            print("your altitude level will be between 0–12 km")
+        elif layer == "troposphere":
+            print("your altitude level will be between 0–12 km")
+
+        else:
+            print("")
+
+    
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
