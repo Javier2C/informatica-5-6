d159bddbc7d932f2613e4765e24b56f62421a67d
Javier2C <juanortiz20500@gmail.com>
2026-09-03 20:09:40 +0000

    game

--- guess-number.py
+++ guess-number.py
@@ -5,20 +5,19 @@ def main():
     name = input(print("what is your name?"))
     print(name)
 
-    number = ""
+    guess = 0
     number = random.randint(1, 100)
     guess = int(input("Guess the number from 1 to 100"))
 
 
 
     while guess != number:
+        guess = int(input("take a guess:"))
         if guess > number:
-            print("higher")
+            print("your guess is too high")
         elif guess < number:
-            print("lower")
-        else guess == number:
-            break
-
+            print("your guess is too low")
+    print(f"good job{name}!")
 
 
 
